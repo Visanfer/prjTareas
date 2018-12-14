eccff2cd70ce9f38aabca39293fb1303bfe5f268
admin visanfer.com <admin@visanfer.com>
2018-12-14 13:23:35 +0100

    .

--- prjTareas/clsTarea.vb
+++ prjTareas/clsTarea.vb
@@ -6,9 +6,18 @@ Public Class clsTarea
 
     Public mnId_Tarea As Integer = 0
     Public mdFecha_Creacion As Date = Now
+    Public mnId_Solicitante As Integer = 0
+    Public mnId_Responsable As Integer = 0
     Public msTitulo As String = ""
+    Public msDescripcion As String = ""
+    Public msId_Estado As String = ""
     Public mdFecha_Inicio_Prevista As Date = CDate("01/01/2000 00:00:00")
-    ' poner el resto de campos en el orden correspondiente
+    Public mdFecha_Fin_Prevista As Date = CDate("01/01/2000 00:00:00")
+    Public mdFecha_Inicio As Date = CDate("01/01/2000 00:00:00")
+    Public mdFecha_Fin As Date = CDate("01/01/2000 00:00:00")
+    Public mnId_Tarea_Padre As Integer = 0
+
+    ' poner el resto de campos en el orden correspondiente (HECHO)
 
     Public mbEsNuevo As Boolean = True
 
@@ -34,8 +43,18 @@ Public Class clsTarea
     Public Sub mrCargaDatos(ByVal loRecord As DataRow)
         mnId_Tarea = CInt(loRecord("id_tarea") & "")
         mdFecha_Creacion = CDate(loRecord("fecha_creacion") & "")
+        mnId_Solicitante = CInt(loRecord("id_solicitante") & "")
+        mnId_Responsable = CInt(loRecord("id_responsable") & "")
         msTitulo = Trim(loRecord("titulo") & "")
-        ' poner el resto de campos en el orden correspondiente
+        msDescripcion = Trim(loRecord("descripcion") & "")
+        msId_Estado = Trim(loRecord("id_estado") & "")
+        mdFecha_Inicio_Prevista = CDate(loRecord("fecha_inicio_prevista") & "")
+        mdFecha_Fin_Prevista = CDate(loRecord("fecha_fin_prevista") & "")
+        mdFecha_Inicio = CDate(loRecord("fecha_inicio") & "")
+        mdFecha_Fin = CDate(loRecord("fecha_fin") & "")
+        mnId_Tarea_Padre = CInt(loRecord("id_tarea_padre") & "")
+
+        ' poner el resto de campos en el orden correspondiente (HECHO)
     End Sub
 
     Public Sub mrGrabaDatos()
@@ -44,16 +63,33 @@ Public Class clsTarea
         Dim loBaseDatos As New clsBaseDatos
 
         If mbEsNuevo Then
-            ' poner el resto de campos en el orden correspondiente
-            lsSql = "insert into tareas(campos,...) values ('" &
+            ' poner el resto de campos en el orden correspondiente (HECHO)
+            lsSql = "insert into tareas(fecha_creacion,id_solicitante,id_responsable,titulo,descripcion,id_estado,fecha_inicio_prevista,fecha_fin_prevista,fecha_inicio,fecha_fin,id_tarea_padre) values ('" &
                         Format(mdFecha_Creacion, formatoFechahora) & "','" &
+                        mnId_Solicitante & "','" &
+                        mnId_Responsable & "','" &
                         msTitulo & "'," &
-                        mnId_Tarea & "); SELECT LAST_INSERT_ID();"
+                        msDescripcion & "','" &
+                        msId_Estado & "','" &
+                        Format(mdFecha_Inicio_Prevista, formatoFechahora) & "','" &
+                        Format(mdFecha_Fin_Prevista, formatoFechahora) & "','" &
+                        Format(mdFecha_Inicio, formatoFechahora) & "','" &
+                        Format(mdFecha_Fin, formatoFechahora) & "','" &
+                        mnId_Tarea_Padre & "); SELECT LAST_INSERT_ID();"
             mnId_Tarea = loBaseDatos.mrEjecutaComandoAI(False, lsSql)
         Else
-            ' poner el resto de campos en el orden correspondiente
-            lsSql = "update tareas set titulo = '" & msTitulo &
+            ' poner el resto de campos en el orden correspondiente (HECHO)
+            lsSql = "update tareas set fecha_creacion = '" & Format(mdFecha_Creacion, formatoFechahora) &
+                    "' id_solicitante = '" & mnId_Solicitante &
+                    "' id_responsable = '" & mnId_Responsable &
+                    "' titulo = '" & msTitulo &
+                    "' descripcion = '" & msDescripcion &
+                    "' id_estado = '" & msId_Estado &
                     "', fecha_inicio_prevista = '" & Format(mdFecha_Inicio_Prevista, formatoFechahora) &
+                    "', fecha_fin_prevista = '" & Format(mdFecha_Fin_Prevista, formatoFechahora) &
+                    "', fecha_inicio = '" & Format(mdFecha_Inicio, formatoFechahora) &
+                    "', fecha_fin = '" & Format(mdFecha_Fin, formatoFechahora) &
+                    "' id_tarea_padre = '" & mnId_Tarea_Padre &
                     "' where id_tarea = " & mnId_Tarea
             loBaseDatos.mrEjecutaComando(False, lsSql)
         End If
